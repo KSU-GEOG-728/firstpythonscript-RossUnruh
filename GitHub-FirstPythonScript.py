735ce46efb073a0bc36607df23b9ae0ac36a679b
Shawn Hutchinson <shutch@ksu.edu>
2024-09-18 09:29:53 -0500

    Instructor Corrections
    Original script ran to completion, but generated incorrect results.
    
    Two important revisions are included.  First, I use the result object generated by line #20 in the buffer tool.  Otherwise it doesn't know to buffer the High Plains ecoregion.  Second, using Statistics quickly summarizes the length of individual rivers to produce a single number.

--- GitHub-FirstPythonScript.py
+++ GitHub-FirstPythonScript.py
@@ -16,13 +16,11 @@ arcpy.env.overwriteOutput = True
 # Set current workspace
 arcpy.env.workspace = r"C:\Users\runru\Desktop\Classes\Fall 2024\GEOG 728\Exercises\Exercise #5\firstpythonscript-RossUnruh\GitHub-FirstPythonScript\ExerciseData.gdb"
 
-# Perform geoprocessing
-
 # Use select by attributes tool to select the name of one of the ecoregions.
 selectEcoregion = arcpy.management.SelectLayerByAttribute('ks_ecoregions', 'NEW_SELECTION', "US_L3NAME = 'High Plains'")
 
 # Create a 10km buffer around the selected ecoregion.
-arcpy.analysis.Buffer('ks_ecoregions', 'outBuffer', '10 kilometers')
+arcpy.analysis.Buffer(selectEcoregion, 'outBuffer', '10 kilometers')
 
 # Clip the river(s) to the new buffer
 arcpy.analysis.Clip('ks_major_rivers', 'outBuffer', 'outClip')
@@ -30,8 +28,5 @@ arcpy.analysis.Clip('ks_major_rivers', 'outBuffer', 'outClip')
 # Add a new column in the outClip shapefile which converts the contents of the Shape_Length column from feet to miles.
 arcpy.management.AddGeometryAttributes('outClip', 'LENGTH', 'MILES_US')
 
-# Copy the new outClip output and rename it totalMiles.
-arcpy.management.CopyFeatures('outClip','totalMiles')
-
-
-
+# Sum lengths in new output table
+arcpy.Statistics_analysis("outClip", "outStats", [["LENGTH", "SUM"]])
